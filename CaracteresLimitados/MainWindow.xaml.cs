f1df32675f6cf1ebd91ef091ba203ae496476955
SusanRojas <sf.rojas16@iesdoctorbalmis.com>
2020-10-27 02:35:14 +0100

    Agregar archivos de proyecto.

---
+++ CaracteresLimitados/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CaracteresLimitados
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cuadroTexBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (cuadroTexBox.Text.Length == 140)
+                cuadroTexBox.IsReadOnly = true;
+
+            for(int i = 0; i <= cuadroTexBox.Text.Length; i++){
+                contadorTextBlock.Text = i + "/140";
+            }
+
+        }
+    }
+}
